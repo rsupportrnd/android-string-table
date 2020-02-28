deb39b5e7155e3c6dcf6bda0d5e34e79fb598dec
SS <developer.kss@gmail.com>
2020-02-28 16:43:25 +0900

    test code kotlin

---
+++ lib/src/test/java/stringtable/TestStringTableGeneratorXml.kt
@@ -0,0 +1,34 @@
+package stringtable
+
+import org.junit.Test
+import java.io.File
+import java.io.IOException
+
+class TestStringTableGeneratorXml {
+    @Test
+    @Throws(Exception::class)
+    fun test() {
+        val fileSource = File("./src/test/java/stringtable/sample/StringTable.xlsx")
+        val resRoot = File("./src/test/java/stringtable/sample/res/")
+        val pathSource: String?
+        val pathRes: String?
+        pathSource = getPathWithFile(fileSource)
+        pathRes = getPathWithFile(resRoot)
+        val args = arrayOf(
+                pathSource,
+                pathRes)
+        StringTableGenerator.main(args)
+    }
+
+    private fun getPathWithFile(file: File): String? {
+        if (file.exists()) {
+            return file.absolutePath
+        }
+        try {
+            return file.canonicalPath
+        } catch (e: IOException) { // TODO Auto-generated catch block
+            e.printStackTrace()
+        }
+        return null
+    }
+}
