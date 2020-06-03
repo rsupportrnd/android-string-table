b439d636659d3a5f6aea997e7e7bdb1ce738beca
Sungshik Kim <kimss@rsupport.com>
2020-06-04 06:47:31 +0900

    support specify sheet name - by kwcho

--- lib/src/main/java/stringtable/StringTableGenerator.java
+++ lib/src/main/java/stringtable/StringTableGenerator.java
@@ -8,32 +8,28 @@ import java.io.FileInputStream;
 import java.security.InvalidParameterException;
 
 public class StringTableGenerator {
-
 	public static void main(String[] args) throws Exception{
-		
-		if (args.length != 2)
+		if (args.length < 2)
 		{
-			throw new InvalidParameterException("<source xlsx> <res path>");
+			throw new InvalidParameterException("<source xlsx> <res path> <sheet name>");
 		}
-		
 		System.out.println("Generate string tables.");
 		System.out.println("\tsource: " + args[0]);
 		System.out.println("\tres: " + args[1]);
-
 		File source = new File(args[0]);
 		File pathRes = new File(args[1]);
-		
-
+		int sheetIndex = 0;
 		FileInputStream is = new FileInputStream(source);
 		XSSFWorkbook workbook = new XSSFWorkbook(is);
-		
-		XSSFSheet sheet = workbook.getSheetAt(0);
-		
+		if(args.length == 3){
+			int findIndex = workbook.getSheetIndex(args[2]);
+			if(findIndex >= 0){
+				sheetIndex = findIndex;
+			}
+		}
+		XSSFSheet sheet = workbook.getSheetAt(sheetIndex);
 		Sheet2Strings.convert(sheet, pathRes);
-		
 		System.out.println("Completed.");
-
 		is.close();
 	}
-
 }
