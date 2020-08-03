12b8f2766fc275c2d9f8778b8cbe77758848e330
Sungshik Kim <kimss@rsupport.com>
2020-08-03 16:11:13 +0900

    test code

--- download/src/test/java/com/jeff/download/google/drive/GoogleDriveDownloadTest.kt
+++ download/src/test/java/com/jeff/download/google/drive/GoogleDriveDownloadTest.kt
@@ -1,4 +1,20 @@
 package com.jeff.download.google.drive
 
+import com.jeff.download.FileDownloader
+import org.junit.Test
+import java.lang.IllegalArgumentException
+
 class GoogleDriveDownloadTest {
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `argument 부족`() {
+
+        FileDownloader.main(arrayOf("xxx"))
+    }
+
+    @Test
+    fun `sample`() {
+        FileDownloader.main(arrayOf("./credentials.json", "12hmQ7U0npYM4hK4ck3qN9bMUDRu-ZcPueluxz5X4w30", "sample"))
+    }
+
 }
