20253f1cb621292fd8cac30f42202454078b2058
Sungshik Kim <kimss@rsupport.com>
2020-08-18 09:08:56 +0900

    add test 5-1

--- lib/src/test/java/stringtable/TestStringTableGeneratorXml.kt
+++ lib/src/test/java/stringtable/TestStringTableGeneratorXml.kt
@@ -30,4 +30,17 @@ class TestStringTableGeneratorXml {
         StringTableGenerator.main(arrayOf(""))
 
     }
+
+    @Test
+    fun `test-5-1`() {
+        val fileSource = File("./src/test/java/stringtable/sample/string-table-starting-5-1.xlsx")
+        val resRoot = File("./src/test/java/stringtable/sample/res-5-1/")
+        val pathSource = getPathWithFile(fileSource)
+        val pathRes = getPathWithFile(resRoot)
+        val args = listOf(
+                pathSource,
+                pathRes)
+        StringTableGenerator.main(args.toTypedArray())
+    }
+
 }
