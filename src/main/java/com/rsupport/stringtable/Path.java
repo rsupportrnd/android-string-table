4973a2e76b2f9a8ccff99eae5757f13534911a4f
kimseulbi <sb900924@naver.com>
2021-03-23 11:34:40 +0900

    패키지 변경

---
+++ src/main/java/com/rsupport/stringtable/Path.java
@@ -0,0 +1,38 @@
+package com.rsupport.stringtable;
+
+public class Path {
+
+	public static String combine(String pathString, String childName) {
+		String pathDelimiter = getDelimiter(pathString + childName);
+
+		String result = pathString;
+
+		if (pathString.endsWith(pathDelimiter))
+			result = pathString;
+		else
+			result = pathString + pathDelimiter;
+
+		if (childName.startsWith(pathDelimiter))
+			result = result + childName.substring(1);
+		else
+			result = result + childName;
+
+		return result;
+	}
+
+	private static String getDelimiter(String path) {
+		if (path.indexOf('\\') != -1)
+			return "\\";
+		else
+			return "/";
+	}
+
+	public static String combine(String pathString, String childName1, String... filenames) {
+		String result = combine(pathString, childName1);
+
+		for (String filename : filenames) {
+			result = combine(result, filename);
+		}
+		return result;
+	}
+}
