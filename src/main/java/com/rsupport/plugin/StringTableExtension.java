5802184ec25134ec6344231b99b7de4c3d08f355
kimseulbi <sb900924@naver.com>
2021-03-24 10:53:00 +0900

    constructor 추가

--- src/main/java/com/rsupport/plugin/StringTableExtension.java
+++ src/main/java/com/rsupport/plugin/StringTableExtension.java
@@ -1,5 +1,6 @@
 package com.rsupport.plugin;
 
+import org.gradle.api.Project;
 import org.gradle.api.provider.Property;
 
 public class StringTableExtension {
@@ -8,4 +9,12 @@ public class StringTableExtension {
     public Property<String> outputExcelFileName;
     public Property<String> outputResourcePath;
     public Property<String> inputSheetName;
+
+    public StringTableExtension(final Project project) {
+        this.googleDriveCredentialPath = project.getObjects().property(String.class);
+        this.spreadSheetFieldId = project.getObjects().property(String.class);
+        this.outputExcelFileName = project.getObjects().property(String.class);
+        this.outputResourcePath = project.getObjects().property(String.class);
+        this.inputSheetName = project.getObjects().property(String.class);
+    }
 }
