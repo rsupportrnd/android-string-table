aaa18ff217ab35f9220efb8fc2b7a5d10b851601
kimseulbi <sb900924@naver.com>
2021-03-24 10:59:59 +0900

    setter 추가

--- src/main/java/com/rsupport/plugin/StringTableExtension.java
+++ src/main/java/com/rsupport/plugin/StringTableExtension.java
@@ -4,6 +4,7 @@ import org.gradle.api.Project;
 import org.gradle.api.provider.Property;
 
 public class StringTableExtension {
+
     public Property<String> googleDriveCredentialPath;
     public Property<String> spreadSheetFieldId;
     public Property<String> outputExcelFileName;
@@ -11,10 +12,30 @@ public class StringTableExtension {
     public Property<String> inputSheetName;
 
     public StringTableExtension(final Project project) {
-        this.googleDriveCredentialPath = project.getObjects().property(String.class);
-        this.spreadSheetFieldId = project.getObjects().property(String.class);
-        this.outputExcelFileName = project.getObjects().property(String.class);
-        this.outputResourcePath = project.getObjects().property(String.class);
-        this.inputSheetName = project.getObjects().property(String.class);
+        this.googleDriveCredentialPath.set(project.getObjects().property(String.class));
+        this.spreadSheetFieldId.set(project.getObjects().property(String.class));
+        this.outputExcelFileName.set(project.getObjects().property(String.class));
+        this.outputResourcePath.set(project.getObjects().property(String.class));
+        this.inputSheetName.set(project.getObjects().property(String.class));
+    }
+
+    public void setGoogleDriveCredentialPath(String googleDriveCredentialPath) {
+        this.googleDriveCredentialPath.set(googleDriveCredentialPath);
+    }
+
+    public void setSpreadSheetFieldId(String spreadSheetFieldId) {
+        this.spreadSheetFieldId.set(spreadSheetFieldId);
+    }
+
+    public void setOutputExcelFileName(String outputExcelFileName) {
+        this.outputExcelFileName.set(outputExcelFileName);
+    }
+
+    public void setOutputResourcePath(String outputResourcePath) {
+        this.outputResourcePath.set(outputResourcePath);
+    }
+
+    public void setInputSheetName(String inputSheetName) {
+        this.inputSheetName.set(inputSheetName);
     }
 }
