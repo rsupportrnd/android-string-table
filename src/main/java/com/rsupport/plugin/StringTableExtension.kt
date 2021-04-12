b1888126726d9f9fbfcc028b9338fd941c3da857
sbkim <sbkim@rsupport.com>
2021-04-12 16:05:47 +0900

    setter 추가

--- src/main/java/com/rsupport/plugin/StringTableExtension.kt
+++ src/main/java/com/rsupport/plugin/StringTableExtension.kt
@@ -1,18 +1,48 @@
 package com.rsupport.plugin
 
+import org.gradle.api.Project
 import org.gradle.api.provider.Property
 
-class StringTableExtension {
+class StringTableExtension(project: Project) {
     @JvmField
-    var googleDriveCredentialPath: Property<String>? = null
+    var googleDriveCredentialPath: Property<String> = project.objects.property(String::class.java)
+
     @JvmField
-    var spreadSheetFieldId: Property<String>? = null
+    var spreadSheetFieldId: Property<String> = project.objects.property(String::class.java)
+
     @JvmField
-    var outputExcelFileName: Property<String>? = null
+    var outputExcelFileName: Property<String> = project.objects.property(String::class.java)
+
     @JvmField
-    var inputExcelFilePath: Property<String>? = null
+    var outputResourcePath: Property<String> = project.objects.property(String::class.java)
+
     @JvmField
-    var outputResourcePath: Property<String>? = null
+    var inputSheetName: Property<String> = project.objects.property(String::class.java)
+
     @JvmField
-    var inputSheetName: Property<String>? = null
+    var indexRowNumber: Property<String> = project.objects.property(String::class.java)
+    fun setGoogleDriveCredentialPath(googleDriveCredentialPath: String?) {
+        this.googleDriveCredentialPath.set(googleDriveCredentialPath)
+    }
+
+    fun setSpreadSheetFieldId(spreadSheetFieldId: String?) {
+        this.spreadSheetFieldId.set(spreadSheetFieldId)
+    }
+
+    fun setOutputExcelFileName(outputExcelFileName: String?) {
+        this.outputExcelFileName.set(outputExcelFileName)
+    }
+
+    fun setOutputResourcePath(outputResourcePath: String?) {
+        this.outputResourcePath.set(outputResourcePath)
+    }
+
+    fun setInputSheetName(inputSheetName: String?) {
+        this.inputSheetName.set(inputSheetName)
+    }
+
+    fun setIndexRowNumber(indexRowNumber: String?) {
+        this.indexRowNumber.set(indexRowNumber)
+    }
+
 }
