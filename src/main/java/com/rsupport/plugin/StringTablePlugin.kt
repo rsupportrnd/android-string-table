910c87cf7cfffe98b8afad0898f5f90117f3ab12
sbkim <sbkim@rsupport.com>
2021-04-21 14:09:10 +0900

    태스크 그룹을 android에서 rsstringupdater로 변경

--- src/main/java/com/rsupport/plugin/StringTablePlugin.kt
+++ src/main/java/com/rsupport/plugin/StringTablePlugin.kt
@@ -8,7 +8,7 @@ class StringTablePlugin : Plugin<Project> {
         val extension = project.extensions.create("stringResourceConfig", StringTableExtension::class.java, project)
         val task = project.tasks.register("updateStringResource", StringTableTask::class.java)
         project.afterEvaluate {
-            task.get().group = "android"
+            task.get().group = "rsstringupdater"
             task.get().googleDriveCredentialPath = extension.googleDriveCredentialPath.get()
             task.get().outputXlsxFilePath = extension.outputXlsxFilePath.get()
             task.get().inputSheetName = extension.inputSheetName.get()
