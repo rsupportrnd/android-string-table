75351fcc5482c3dcf811c70135101b61fc7ec465
sbkim <sbkim@rsupport.com>
2021-05-27 15:57:51 +0900

    generate xml file task 추가

---
+++ src/main/java/com/rsupport/plugin/GenerateXmlTask.kt
@@ -0,0 +1,30 @@
+package com.rsupport.plugin
+
+import com.rsupport.google.GoogleCredentials
+import com.rsupport.google.sheet.SheetUrlParser
+import com.rsupport.stringtable.StringTableGenerator
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+open class GenerateXmlTask : DefaultTask() {
+
+    var googleDriveCredentialPath = ""
+    var targetSheetUrl = ""
+    var outputXlsxFilePath = ""
+    var androidResourcePath = ""
+    var indexRowNumber : Int? = null
+    var outputXmlFileName : String? = null
+
+    @TaskAction
+    fun generateXml() {
+        val credential = GoogleCredentials.createCredentials(googleDriveCredentialPath)
+        val sheetURLParser = SheetUrlParser(credential, targetSheetUrl)
+        StringTableGenerator.generate(
+            outputXlsxFilePath,
+            androidResourcePath,
+            sheetURLParser.sheetName,
+            indexRowNumber,
+            outputXmlFileName
+        )
+    }
+}
--- src/main/java/com/rsupport/plugin/StringTablePlugin.kt
+++ src/main/java/com/rsupport/plugin/StringTablePlugin.kt
@@ -7,7 +7,7 @@ class StringTablePlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val extension = project.extensions.create("androidStringTable", StringTableExtension::class.java, project)
 
-        val downloadExcelFileTask = project.tasks.register("downloadSpreadSheet", DownloadExcelFileTask::class.java)
+        val downloadExcelFileTask = project.tasks.register("downloadSpreadsheet", DownloadExcelFileTask::class.java)
         project.afterEvaluate {
             downloadExcelFileTask.get().group = "androidStringTable"
             downloadExcelFileTask.get().googleDriveCredentialPath = extension.googleDriveCredentialPath.get()
@@ -15,7 +15,7 @@ class StringTablePlugin : Plugin<Project> {
             downloadExcelFileTask.get().targetSheetUrl = extension.targetSheetUrl.get()
         }
 
-        val generateXmlTask = project.tasks.register("createStringsXmlFile", StringTableTask::class.java)
+        val generateXmlTask = project.tasks.register("generateStringsXmls", GenerateXmlTask::class.java)
         project.afterEvaluate {
             generateXmlTask.get().group = "androidStringTable"
             generateXmlTask.get().googleDriveCredentialPath = extension.googleDriveCredentialPath.get()
@@ -24,6 +24,17 @@ class StringTablePlugin : Plugin<Project> {
             generateXmlTask.get().indexRowNumber = extension.indexRowNumber.get()
             generateXmlTask.get().targetSheetUrl = extension.targetSheetUrl.get()
             generateXmlTask.get().outputXmlFileName = extension.outputXmlFileName.get()
+        }
+
+        val downloadAndGenerateTask = project.tasks.register("downloadSheetAndGenerateXmls", StringTableTask::class.java)
+        project.afterEvaluate {
+            downloadAndGenerateTask.get().group = "androidStringTable"
+            downloadAndGenerateTask.get().googleDriveCredentialPath = extension.googleDriveCredentialPath.get()
+            downloadAndGenerateTask.get().outputXlsxFilePath = extension.outputXlsxFilePath.get()
+            downloadAndGenerateTask.get().androidResourcePath = extension.androidResourcePath.get()
+            downloadAndGenerateTask.get().indexRowNumber = extension.indexRowNumber.get()
+            downloadAndGenerateTask.get().targetSheetUrl = extension.targetSheetUrl.get()
+            downloadAndGenerateTask.get().outputXmlFileName = extension.outputXmlFileName.get()
         }
 
     }
