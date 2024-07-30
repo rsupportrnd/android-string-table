66a044d802e678bc4360ae59bf30b5d894f5a638
ssk <developer.kss@gmail.com>
2024-07-30 20:50:56 +0900

    feat(#0):values- 로 시작하지 않는 언어코드 추출
    #body
    #bottom
    ###
    # feat      : 기능 추가, 삭제, 변경
    # fix       : 버그 수정
    # refactor  : 리팩토링
    ### 동작 변경 X
    # style     : 코드 형식 정렬
    # test      : 테스트 코드 추가, 삭제, 변경
    ### 코드 수정 X
    # docs      : 문서 추가, 삭제, 변경
    # chore     : 빌드 스크립트, 패키징 관련 변경

---
+++ src/main/java/com/rsupport/stringtable/LanguageCode.kt
@@ -0,0 +1,21 @@
+package com.rsupport.stringtable
+
+import java.util.*
+
+class LanguageCode {
+    companion object {
+        fun isValid(languageCode: String): Boolean {
+            val availableLanguageCodes = Locale.getAvailableLocales().map { it.toLanguageTag() }.toSet()
+
+            if (languageCode.startsWith("values")) {
+                val actualCode = languageCode.removePrefix("values-")
+                return availableLanguageCodes.contains(actualCode)
+            }
+
+            return availableLanguageCodes.contains(languageCode)
+
+        }
+    }
+
+
+}
